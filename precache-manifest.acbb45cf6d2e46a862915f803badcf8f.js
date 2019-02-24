c69ab2302718cd0bef31c3d2d2c07da001a721b2
Gabe R <rodriguezgab1601@gmail.com>
2019-02-24 18:39:19 +0800

    Updates

---
+++ precache-manifest.acbb45cf6d2e46a862915f803badcf8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b77e268e2f58503be06d",
+    "url": "/maki/static/js/runtime~main.b77e268e.js"
+  },
+  {
+    "revision": "8b1b36f15e406d1c39d4",
+    "url": "/maki/static/js/main.8b1b36f1.chunk.js"
+  },
+  {
+    "revision": "bb1cfbc77e0591448a8c",
+    "url": "/maki/static/js/2.bb1cfbc7.chunk.js"
+  },
+  {
+    "revision": "8b1b36f15e406d1c39d4",
+    "url": "/maki/static/css/main.914d2657.chunk.css"
+  },
+  {
+    "revision": "e9ccf3b1bb25895b47dcfa45e860893c",
+    "url": "/maki/index.html"
+  }
+];
